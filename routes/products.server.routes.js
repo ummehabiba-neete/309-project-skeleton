629d36b0f11d070c477db6f2e062493e89748fca
Neete <1431279@iub.edu.bd>
2017-11-12 16:33:01 +0000

    assignment13

---
+++ routes/products.server.routes.js
@@ -0,0 +1,41 @@
+module.exports = function(app){
+
+ var products = require('./../controllers/products.server.controller.js');
+ var users = require('./../controllers/users.server.controller.js');
+
+ app.route('/products/new')
+	.get(products.new);
+	//.post(users.requiresLogin, products.create);
+
+  app.route('/products/all')
+	.get(products.all);
+  //.delete(users.requiresLogin, products.delete);
+  
+  app.route('/products/edit/:productId')
+	.get(products.edit);
+  //.delete(users.requiresLogin, products.delete);
+   app.route('/products/:productId')
+	.get(products.view)
+	//.put(users.requiresLogin, products.update);
+	 
+  //.delete(users.requiresLogin, products.delete);
+
+	app.route('/api/products/')
+	.get(products.list)
+ 	.post(users.requiresLogin, products.create);
+
+//	.put(users.requiresLogin, products.update);
+	 
+ 
+		 app.route('/api/products/edit/:productId')
+	.get(products.read)
+		.put(users.requiresLogin, products.delete);
+
+		 app.route('/api/products/edit/"productId')
+	.get(products.read)
+		.put(users.requiresLogin, products.update);
+
+app.param('productId', products.productByID);
+
+
+}
